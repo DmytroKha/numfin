3e179e8c9101d72e1fe0021613c997dac0f91547
DmytroKha <dmytro_khlypun@ukr.net>
2023-09-07 17:38:08 +0300

    refactor

--- numfin.go
+++ numfin.go
@@ -11,6 +11,7 @@ const (
 	openaiUrl = "https://api.openai.com/v1/chat/completions"
 	apiKey    = "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" //"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" //
 	modelGPT  = "gpt-3.5-turbo"
+	prompt    = "Add 4 sentences to this text giving analytics"
 )
 
 func MakeAnalyticsFromTheCompany(code string) (string, error) {
@@ -25,7 +26,6 @@ func MakeAnalyticsFromTheCompany(code string) (string, error) {
 
 	text := financialDataService.GenerateText(data)
 
-	prompt := "Add 4 sentences to this text giving analytics"
 	gpt3Response, err := openaiService.GenerateResponse(modelGPT, text+"\n"+prompt)
 	if err != nil {
 		return "", fmt.Errorf("Error generating GPT-3 response for %s: %v\n", code, err)
